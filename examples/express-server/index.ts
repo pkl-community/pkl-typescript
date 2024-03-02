ed88893bcb558f5290254ab7c61403836be7be1f
Jason Gwartz <jason@gwartz.me>
2024-03-02 18:29:19 +0000

    Add an examples directory, with a first example of a simple express server

---
+++ examples/express-server/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { loadFromPath } from "./generated/config_schema.pkl";
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("Hello from a pkl-configured app!");
+});
+
+// The config file to evaluate can be dynamically chosen based on the value of NODE_ENV
+const configFile = `config.${process.env.NODE_ENV ?? "dev"}.pkl`;
+
+// Use pkl-typescript to load and evaluate the selected Pkl file
+loadFromPath(configFile).then((config) => {
+  console.log("Loaded config values from Pkl:", JSON.stringify(config));
+
+  // `config` is a typed object, of the schema given in ConfigSchema.pkl
+  app.listen(config.port, config.address, () => {
+    console.log("Server started");
+  });
+});
