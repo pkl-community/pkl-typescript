8242457786b41d15441ce128344a10ea06d26a04
Ashley Engelund (weedySeaDragon @ github) <ashley.engelund@gmail.com>
2025-08-10 10:58:36 -0700

    add a jest config to ignore dist dir; make test matches dirs explicit

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,10 @@ module.exports = {
   moduleNameMapper: {
     "^@pkl-community/pkl-typescript$": "<rootDir>/src",
   },
+  testMatch: [
+    "<rootDir>/codegen/**/*.test.ts",
+    "<rootDir>/e2e/**/*.ts",
+    "<rootDir>/src/**/*.test.ts",
+    "!**/dist/**",
+  ]
 };
